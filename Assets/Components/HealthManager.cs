5cbaf1849607f5b29f10c80ac27bbb922f56e58a
Pritesh <pritesh-@outlook.com>
2015-10-07 22:41:11 +0100

    Added health component

---
+++ Assets/Components/HealthManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+	public int maxHealth;
+	public int currentHealth;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void TakeDamage(int dmg)
+	{
+		currentHealth -= dmg;
+	}
+	
+	public void AddHealth(int health)
+	{
+		currentHealth += health;
+		if (currentHealth > maxHealth) 
+		{
+			currentHealth = maxHealth;
+		}
+		
+	}
+}
