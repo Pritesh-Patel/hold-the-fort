9e89c4f35f050ac14d6bc28556dd4730e1758874
Pritesh <pritesh-@outlook.com>
2015-10-12 10:06:33 +0100

    Defender user control script, currently uses gizmos to show maxand min fire angle in editor

---
+++ Assets/Components/DefenderUserController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DefenderUserController : MonoBehaviour {
+
+	// Use this for initialization
+	public List<GameObject> defenders;
+	public float maxTopAngle;
+	public float minBotAngle;
+	public bool showDebugAngles;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	public void AimAtMouse(GameObject def)
+	{
+		Transform defTran = def.transform;
+
+
+	}
+
+	public void ShowDebugLines()
+	{
+		foreach (GameObject def in defenders) {
+
+			Quaternion rotMin = Quaternion.AngleAxis (minBotAngle, Vector3.forward);
+			Quaternion rotMax = Quaternion.AngleAxis (maxTopAngle, Vector3.forward);
+
+			Vector3 botLine = rotMin * Vector3.right;
+			Vector3 topLine = rotMax * Vector3.right;
+
+			Gizmos.color = Color.red;
+			Gizmos.DrawRay (def.transform.position, botLine);
+			Gizmos.DrawRay (def.transform.position, topLine);
+		}
+
+	}
+
+	void OnDrawGizmos()
+	{
+		if (showDebugAngles)
+			ShowDebugLines ();
+	}
+
+}
