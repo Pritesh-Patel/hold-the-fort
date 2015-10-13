94838b1c7726c58d9b9267a77b40711013ee2be5
Rob Darby <darbyrob@hotmail.co.uk>
2015-10-13 22:11:47 +0100

    add credits scene

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour 
+{	
+	public void redirectToScene(string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+}
