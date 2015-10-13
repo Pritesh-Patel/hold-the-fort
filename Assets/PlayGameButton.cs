981b9aae5096fbfe24013a01595b01f9925123d8
Rob Darby <darbyrob@hotmail.co.uk>
2015-10-13 21:29:14 +0100

    Add login scene and script to play game

---
+++ Assets/PlayGameButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGameButton : MonoBehaviour 
+{	
+	public void PlayButton(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+}
