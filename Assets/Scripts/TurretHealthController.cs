1d1e435cff2d471bf371e6f8f144a6ac962a3a16
Rob Darby <darbyrob@hotmail.co.uk>
2015-11-18 21:38:20 +0000

    intial commit for hud- health bar

---
+++ Assets/Scripts/TurretHealthController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretHealthController : MonoBehaviour {
+
+	public float max_Health = 100f;
+	public float current_Health = 0f;
+	public GameObject healthBar;
+
+	// Use this for initialization
+	void Start () {
+		current_Health = max_Health;
+		InvokeRepeating ("decreaseHealth", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void decreaseHealth() {
+		current_Health -= 2f;
+		float calculate_Health = current_Health / max_Health;
+		setHealthBar (calculate_Health);
+	}
+
+	public void setHealthBar(float myHealth) {
+		healthBar.transform.localScale = new Vector3 (myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+	}
+}
