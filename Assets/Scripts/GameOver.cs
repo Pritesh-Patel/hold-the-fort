d4abf202e40a39fbbe69b7904f5575f085a17a1c
Andy <andy.burdis@yahoo.com>
2015-11-18 22:40:44 +0000

    Added new game over screen

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+	public Health health;     
+	public float restartDelay = 5f;         
+		
+	Animator anim;                          
+	float restartTimer;                     
+
+	void Awake ()
+	{
+		// Set up the reference.
+		anim = GetComponent <Animator> ();
+	}
+	
+	
+	void Update ()
+	{
+		if(health.currentHealth <= 0)
+		{
+			anim.SetTrigger ("GameOver");
+			
+			restartTimer += Time.deltaTime;
+			
+			if(restartTimer >= restartDelay)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
+}
